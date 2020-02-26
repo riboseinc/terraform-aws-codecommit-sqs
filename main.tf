b68530f3f77bec8d27a7bd26da038cba1a1afcec
Justin Seiser <justinseiser@gmail.com>
2020-02-26 09:47:09 -0500

    Terraform 12

--- main.tf
+++ main.tf
@@ -1,22 +1,22 @@
 resource "aws_codecommit_repository" "main" {
-  repository_name = "${var.reponame}"
-  description = "CodeCommit repo: ${var.reponame}"
-  default_branch = "master"
+  repository_name = var.reponame
+  description     = "CodeCommit repo: ${var.reponame}"
+  default_branch  = "master"
 }
 
 resource "aws_codecommit_trigger" "main" {
-  repository_name = "${aws_codecommit_repository.main.repository_name}"
+  repository_name = aws_codecommit_repository.main.repository_name
 
   trigger {
-    name = "notifications"
-    events = ["all"]
-    destination_arn = "${aws_sns_topic.main.arn}"
+    name            = "notifications"
+    events          = ["all"]
+    destination_arn = aws_sns_topic.main.arn
   }
 
   trigger {
-    name = "email"
-    events = ["all"]
-    destination_arn = "${var.email-sns-arn}"
+    name            = "email"
+    events          = ["all"]
+    destination_arn = var.email-sns-arn
   }
-
 }
+
